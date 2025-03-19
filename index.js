c36d4d59991f47e07bcea1961343674bdbadb590
Sanjana Kashyap <kashyap.san@northeastern.edu>
2025-03-18 23:23:34 -0400

    need to create module

---
+++ index.js
@@ -0,0 +1,10 @@
+import express from 'express'
+import Hello from './hello.js'
+import Lab5 from './Lab5/index.js';
+const app = express()
+
+Hello(app);
+Lab5(app);
+
+app.listen(process.env.PORT || 4000)
+
