55066e225fa6d72ba05d251eba4e40e3493eeb85
Sanjana Kashyap <kashyap.san@northeastern.edu>
2025-03-19 12:28:50 -0400

    add module editing endpoints

--- Lab5/WorkingWithObjects.js
+++ Lab5/WorkingWithObjects.js
@@ -4,6 +4,12 @@ const assignment = {
   due: "2021-10-10", completed: false, score: 0,
 };
 
+const module = {
+  id: 2, name: "NodeJS Module",
+  description: "Adding a new endpoint to fetch objects",
+  course: "CS 1210"
+}
+
 export default function WorkingWithObjects(app) {
   app.get("/lab5/assignment", (req, res) => {
     res.json(assignment);
@@ -15,5 +21,23 @@ export default function WorkingWithObjects(app) {
     const { newTitle } = req.params;
     assignment.title = newTitle;
     res.json(assignment);
+  });
+  app.get("/lab5/assignment/completed/:completed", (req, res) => {
+    console.log(req.params);
+    const { completed } = req.params;
+    assignment.completed = JSON.parse(completed);
+    res.json(assignment);
+  });
+  app.get("/lab5/assignment/score/:newScore", (req, res) => {
+    const { newScore } = req.params;
+    assignment.score = JSON.parse(newScore);
+    console.log(newScore);
+    res.json(assignment);
+  });
+  app.get("/lab5/module", (req, res) => {
+    res.json(module);
+  });
+  app.get("/lab5/module/name", (req, res) => {
+    res.json(module.name);
   });
 };
