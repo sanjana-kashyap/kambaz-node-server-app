6d200b9faa4732bfd7a141f3bc827ee90c39c009
Sanjana Kashyap <kashyap.san@northeastern.edu>
2025-04-02 12:08:57 -0400

    starting kambaz node server

--- Lab5/WorkingWithArrays.js
+++ Lab5/WorkingWithArrays.js
@@ -26,17 +26,50 @@ export default function WorkingWithArrays(app) {
     res.json(todos);
   });
 
+  app.post("/lab5/todos", (req, res) => {
+    const newTodo = { ...req.body, id: new Date().getTime() };
+    todos.push(newTodo);
+    res.json(newTodo);
+  });
+
   app.get("/lab5/todos/:id", (req, res) => {
     const { id } = req.params;
     const todo = todos.find((t) => t.id === parseInt(id));
     res.json(todo);
   });
 
+  app.put("/lab5/todos/:id", (req, res) => {
+    const { id } = req.params;
+    const todoIndex = todos.findIndex((t) => t.id === parseInt(id));
+    if (todoIndex === -1) {
+      res.status(404).json({ message: `Unable to update Todo with ID ${id}` });
+      return;
+    }
+    todos = todos.map((t) => {
+      if (t.id === parseInt(id)) {
+        return { ...t, ...req.body };
+      }
+      return t;
+    });
+    res.sendStatus(200);
+  });
+
   app.get("/lab5/todos/:id/delete", (req, res) => {
     const { id } = req.params;
     const todoIndex = todos.findIndex((t) => t.id === parseInt(id));
+    if (todoIndex === -1) {
+      res.status(404).json({ message: `Unable to delete Todo with ID ${id}` });
+      return;
+    }
     todos.splice(todoIndex, 1);
     res.json(todos);
+  });
+
+  app.delete("/lab5/todos/:id", (req, res) => {
+    const { id } = req.params;
+    const todoIndex = todos.findIndex((t) => t.id === parseInt(id));
+    todos.splice(todoIndex, 1);
+    res.sendStatus(200);
   });
 
   app.get("/lab5/todos/:id/title/:title", (req, res) => {
--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 import express from 'express'
 import Hello from './hello.js'
 import Lab5 from './Lab5/index.js';
-const app = express()
+import cors from "cors";
+
+const app = express();
+
+console.log("starting up server....");
+
+app.use(cors());
+app.use(express.json());
 
 Hello(app);
 Lab5(app);
 
-app.listen(process.env.PORT || 4000)
+app.listen(4000, () => console.log("Server running on port 4000!!"));
 
