b011399bb9eec7fef3d7029b4ea7939c66c07759
Sanjana Kashyap <100091218+sanjana-kashyap@users.noreply.github.com>
2025-04-08 20:30:43 -0400

    Update index.js

--- index.js
+++ index.js
@@ -47,5 +47,5 @@ ModuleRoutes(app);
 AssignmentRoutes(app);
 EnrollmentRoutes(app);
 
-app.listen(4000, () => console.log("Server running on port 4000!!"));
+app.listen(process.env.PORT, () => console.log("Server running on port 4000!!"));
 
